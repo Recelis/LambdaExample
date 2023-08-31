f13b984556d923d16407c597a2ff277578b21c94
Jacky Lui <jackymlui@live.com>
2023-09-01 06:37:15 +1000

    handler using js file

--- cdk/lib/backend-stack.ts
+++ cdk/lib/backend-stack.ts
@@ -1,5 +1,6 @@
 import * as cdk from 'aws-cdk-lib';
 import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as nodejs from 'aws-cdk-lib/aws-lambda-nodejs';
 import * as apigateway from 'aws-cdk-lib/aws-apigateway'
 import { Construct } from 'constructs';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
@@ -8,14 +9,23 @@ export class BackendStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    
+
 
     const expressLambda = new lambda.Function(this, 'ExpressLambda', {
       functionName: 'backend',
       runtime: lambda.Runtime.NODEJS_18_X,
-      handler: 'lambda.handler',
-      code: lambda.Code.fromAsset('../app'),
+      handler: 'lambda.handler', // cannot find a typescript file?
+      code: lambda.Code.fromAsset('../app/build'),
     });
+
+    // building errors using nodejs?
+    // const expressLambda = new nodejs.NodejsFunction(this, 'ExpressLambda', {
+    //   functionName: 'backend',
+    //   runtime: lambda.Runtime.NODEJS_18_X,
+    //   handler: 'lambda.handler',
+    //   entry: '../app/build/lambda.js',
+    //   depsLockFilePath: '../app/yarn.lock'
+    // });
 
     // Create an API Gateway
     const api = new apigateway.RestApi(this, 'ExpressApi');
