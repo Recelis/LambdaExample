1775f925e40147dad61a975439acb3812e9d96c6
Jacky Lui <jackymlui@live.com>
2024-04-25 10:47:38 +1000

    added custom integration lamba names

--- cdk/lib/backend-stack.ts
+++ cdk/lib/backend-stack.ts
@@ -2,13 +2,14 @@ import * as cdk from "aws-cdk-lib";
 import * as lambda from "aws-cdk-lib/aws-lambda";
 import * as apigateway from "aws-cdk-lib/aws-apigateway";
 import { Construct } from "constructs";
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class BackendStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const expressLambda = new lambda.Function(this, "ExpressLambda", {
+    const stackName = cdk.Stack.of(this).stackName;
+
+    const expressLambda = new lambda.Function(this, `${stackName}ExpressLambda`, {
       functionName: "backend",
       runtime: lambda.Runtime.NODEJS_18_X,
       handler: "build/lambda.handler", // always look for the build directory
@@ -16,7 +17,7 @@ export class BackendStack extends cdk.Stack {
     });
 
     // Create an API Gateway
-    const api = new apigateway.RestApi(this, "ExpressApi");
+    const api = new apigateway.RestApi(this, `${stackName}ExpressApi`);
 
     // Create an integration using aws-serverless-express
     const integration = new apigateway.LambdaIntegration(expressLambda, {
